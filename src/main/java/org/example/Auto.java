54434b6fdde988a5338368d7c4201fefb66fc01d
ITulian <ignacio.tulian@mercadolibre.com>
2024-02-06 17:57:56 -0300

    dakar

---
+++ src/main/java/org/example/Auto.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Auto extends Vehiculo{
+
+
+    public Auto(int velocidad, int aceleracion, double anguloGiro, String patente) {
+        super(velocidad, aceleracion, anguloGiro, patente, 1000, 4);
+    }
+}
---
+++ src/main/java/org/example/Moto.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Moto extends Vehiculo{
+
+    public Moto(int velocidad, int aceleracion, double anguloGiro, String patente) {
+        super(velocidad, aceleracion, anguloGiro, patente, 300, 2);
+    }
+}
